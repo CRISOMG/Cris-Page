ffb3e2ab9c369d0692d96c95ab896b8936a73da9
Cristian <cristiancaraballo112@gmail.com>
2020-12-15 16:50:45 -0400

    intentando arreglar la miniatura de los meta tags en whatsapp

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,52 +8,30 @@ export default function Home() {
         <title>Cris Page</title>
         <link rel='icon' href='/favicon.ico' />
         {/* <title>Cristian Caraballo - Frontend Developer with React.js | Next.js /</title> */}
-        <meta
-          key='title'
-          name='title'
-          content='Cristian Caraballo - Frontend Developer with React.js | Next.js '
-        />
-        <meta
-          key='description'
-          name='description'
-          content='Desarrollador Web especialista en Frontend Development. Tecnologías/Metodologías: MERN & JAM Stack. visita mi pagina para saber más...'
-        />
+        <meta key='title' name='title' content='Cristian Caraballo - Frontend Developer with React.js | Next.js ' />
+        <meta key='description' name='description' content='Desarrollador Web especialista en Frontend Development. Tecnologías/Metodologías: MERN & JAM Stack. visita mi pagina para saber más...' />
 
         <meta key='og:type' property='og:type' content='website' />
+        <meta key='og:site_name' property='og:site_name' content='Cris Page' />
         <meta key='og:url' property='og:url' content='https://cris-page.vercel.app/' />
-        <meta
-          key='og:title'
-          property='og:title'
-          content='Cristian Caraballo - Frontend Developer with React.js | Next.js '
-        />
-        <meta
-          key='og:description'
-          property='og:description'
-          content='Desarrollador Web especialista en Frontend Development. Tecnologías/Metodologías: MERN & JAM Stack. visita mi pagina para saber más...'
-        />
-        <meta
-          key='og:image'
-          property='og:image'
-          content='https://cris-page.vercel.app/assets/meta-image.jpg'
-        />
+        <meta key='og:title' property='og:title' content='Cristian Caraballo - Frontend Developer with React.js | Next.js ' />
+        <meta key='og:description' property='og:description' content='Desarrollador Web especialista en Frontend Development. Tecnologías/Metodologías: MERN & JAM Stack. visita mi pagina para saber más...' />
+        {/* <meta key='og:image' property='og:image' content='https://cris-page.vercel.app/assets/meta-image-1200x628.jpg' />
+        <meta key='og:image:type:1200x628' property='og:image:type' content='image/jpeg' />
+        <meta key='og:image:width:1200' property='og:image:width' content='1200' />
+        <meta key='og:image:height:628' property='og:image:height' content='628' /> */}
+
+        <meta key='og:image:whatsapp' property='og:image' content='https://cris-page.vercel.app/assets/meta-image-whatsapp.jpg' />
+        <meta key='og:image:secure:whatsapp' property='og:image:secure_url' content='https://cris-page.vercel.app/assets/meta-image-whatsapp.jpg' />
+        <meta key='og:image:type:whatsapp' property='og:image:type' content='image/jpeg' />
+        <meta key='og:image:width:900' property='og:image:width' content='900' />
+        <meta key='og:image:height:660' property='og:image:height' content='660' />
 
         <meta key='twitter:card' property='twitter:card' content='summary_large_image' />
         <meta key='twitter:url' property='twitter:url' content='https://cris-page.vercel.app/' />
-        <meta
-          key='twitter:title'
-          property='twitter:title'
-          content='Cristian Caraballo - Frontend Developer with React.js | Next.js '
-        />
-        <meta
-          key='twitter:description'
-          property='twitter:description'
-          content='Desarrollador Web especialista en Frontend Development. Tecnologías/Metodologías: MERN & JAM Stack. visita mi pagina para saber más...'
-        />
-        <meta
-          key='twitter:image'
-          property='twitter:image'
-          content='https://cris-page.vercel.app/assets/meta-image.jpg'
-        />
+        <meta key='twitter:title' property='twitter:title' content='Cristian Caraballo - Frontend Developer with React.js | Next.js ' />
+        <meta key='twitter:description' property='twitter:description' content='Desarrollador Web especialista en Frontend Development. Tecnologías/Metodologías: MERN & JAM Stack. visita mi pagina para saber más...' />
+        <meta key='twitter:image' property='twitter:image' content='https://cris-page.vercel.app/assets/meta-image-1200x628.jpg' />
       </Head>
 
       <main className='flex flex-col items-center text-center p-4 min-h-screen'>
@@ -61,24 +39,12 @@ export default function Home() {
           <h2 className='_font-grand-hotel text-5xl'>Cristian</h2>
         </div>
         <div className='my-4'>
-          <img
-            width='240'
-            height='240'
-            className='mx-auto rounded-full w-60'
-            src='/assets/cris-photo.jpg'
-            alt='Cristian - Foto de perfil'
-          />
+          <img width='240' height='240' className='mx-auto rounded-full w-60' src='/assets/cris-photo.jpg' alt='Cristian - Foto de perfil' />
         </div>
         <div className='text-center mb-4'>
           <h1 className='_font-grand-hotel text-5xl'>Frontend Developer</h1>
           <div className='flex ml-4'>
-            <img
-              className='object-contain -mb-1'
-              width='25'
-              height='25'
-              src='/assets/platzi-icon.png'
-              alt='platzi logo'
-            />
+            <img className='object-contain -mb-1' width='25' height='25' src='/assets/platzi-icon.png' alt='platzi logo' />
             <h2 className='_font-arial text-2xl pt-2 italic'>Platzi Eternal Student</h2>
           </div>
           <h3 className='_font-nova-round font-bold italic text-xl '>Nerver Stop Learner</h3>
