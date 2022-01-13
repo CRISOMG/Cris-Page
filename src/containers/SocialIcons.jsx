327d90001fd0c7b5831bbfdbb818ef296cd4134c
Cristian <cristiancaraballo112@gmail.com>
2022-01-13 19:57:33 -0400

    figma icon link at the link tree

--- src/containers/SocialIcons.jsx
+++ src/containers/SocialIcons.jsx
@@ -1,10 +1,13 @@
-import { GithubIcon, TwitterIcon, PlatziIcon, LinkedinIcon } from '../components/svg-icons';
+import { GithubIcon, TwitterIcon, PlatziIcon, LinkedinIcon, FigmaIcon } from '../components/svg-icons';
 
 export default function SocialIcons() {
   return (
     <div className='flex justify-around items-center mx-auto w-full max-w-sm h-32 '>
       <a href='https://twitter.com/_crisomg' target='_blank' rel='noopener noreferrer'>
         <TwitterIcon />
+      </a>
+      <a href='https://figma.com/@CrisOMg/' target='_blank' rel='noopener noreferrer'>
+        <FigmaIcon />
       </a>
       <a href='https://platzi.com/@CrisOMG/' target='_blank' rel='noopener noreferrer'>
         <PlatziIcon />
