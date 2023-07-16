2c8b2ea1657a7ae57dfb79f8c853f9ace4d3a434
Cristian Caraballo <caraballodev@gmail.com>
2023-07-16 13:53:27 -0400

    featuring

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,60 @@
-import '../styles/globals.css';
+import Script from "next/script";
+import "../styles/globals.css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    if (window) {
+      const handleGoogle = async (response) => {
+        console.log(response.credential);
+        // setLoading(true);
+        // fetch(url, {
+        //   method: "POST",
+        //   headers: {
+        //     "Content-Type": "application/json",
+        //   },
+
+        //   body: JSON.stringify({ credential: response.credential }),
+        // })
+        //   .then((res) => {
+        //     setLoading(false);
+
+        //     return res.json();
+        //   })
+        //   .then((data) => {
+        //     if (data?.user) {
+        //       localStorage.setItem("user", JSON.stringify(data?.user));
+        //       window.location.reload();
+        //     }
+
+        //     throw new Error(data?.message || data);
+        //   })
+        //   .catch((error) => {
+        //     setError(error?.message);
+        //   });
+      };
+      window.onload = function () {
+        if (window.google) {
+          google.accounts.id.initialize({
+            client_id: process.env.NEXT_PUBLIC_GOOGLE_ID,
+            callback: handleGoogle,
+          });
+          google.accounts.id.prompt();
+        }
+      };
+    }
+  }, []);
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        src="https://accounts.google.com/gsi/client"
+        strategy="beforeInteractive"
+      />
+      {/* <script src="https://accounts.google.com/gsi/client" async></script> */}
+    </>
+  );
 }
 
 export default MyApp;
---
+++ src/pages/login.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import SocialIcons from "../containers/SocialIcons";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Cris Page | Login</title>
+      </Head>
+      <main>Login</main>
+    </div>
+  );
+}
