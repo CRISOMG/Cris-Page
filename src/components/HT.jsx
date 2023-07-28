ab785ce80d1a797440608581410c6b578e5eb30e
Cristian <cristiancaraballo112@gmail.com>
2023-07-28 17:04:07 -0400

    super diego

---
+++ src/components/HT.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+export function HT({ children }) {
+  return (
+    <span className="font-bold text-green-400 italic mx-[2px]">{children}</span>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+import { HT } from "./../components/HT";
 import Head from "next/head";
 import SocialIcons from "../containers/SocialIcons";
 
@@ -66,8 +67,8 @@ export default function Home() {
           content="https://cris-page.vercel.app/assets/meta-image.jpg"
         />
       </Head>
-      <main className="overflow-y-auto snap-mandatory snap-y max-h-screen scroll-smooth">
-        <hr className="snap-mandatorysnap-y snap-start" />
+      <main className="overflow-y-auto snap-proximity snap-y max-h-screen scroll-smooth">
+        <hr className="snap-y snap-start snap-proximity" />
 
         <section className=" cris-main-bg-section flex flex-col justify-center items-center text-center p-4 min-h-screen">
           <div className="">
@@ -113,13 +114,13 @@ export default function Home() {
 
         <hr className="snap-mandatorysnap-y snap-start" />
 
-        <section className="bg-gradient-to-b from-blue-400 to-blue-600  flex flex-col justify-center items-center text-center p-4 min-h-screen">
-          <div className="rounded-lg md:w-1/2 p-4  flex-col items-center bg-black/20">
-            <h3 className="text-amber-300 md:text-4xl font-bold  text-left">
+        <section className="bg-gradient-to-b from-gray-800 to-gray-900  flex flex-col justify-center items-center text-center min-h-screen">
+          <div className="px-10 py-7 rounded-lg md:w-1/2 flex-col items-center bg-gradient-to-b from-black/40 to-black/25 shadow-md border-2 border-white/20">
+            <h3 className="text-blue-400 md:text-4xl font-bold  text-left">
               Perfil Profesional
             </h3>
             <br />
-            <p className="text-white md:text-lg  text-left">
+            <p className="text-gray-300 md:text-lg  text-left">
               Experto con casi 3 años de experiencia en el manejo de tecnologías
               del ecosistema JavaScript­ principalmente con el Stack MERN y
               NextJS. He colaborado en 7 proyectos implementando las tecnologías
@@ -131,19 +132,20 @@ export default function Home() {
             </p>
             <br />
 
-            <p className="text-white md:text-lg  text-left">
+            <p className="text-gray-300 md:text-lg  text-left">
               Aspiro a ser un Desarrollador Web FullStack con Node.js de alto
               rendimiento e impacto capaz de diseñar e implementar soluciones de
               software al mundo.
             </p>
             <br />
-            <h3 className="text-amber-300 md:text-4xl font-bold  text-left">
+            <hr className="opacity-20" />
+            <h3 className="text-blue-400 md:text-4xl mt-7 font-bold  text-left">
               Cualidades y Aptitudes
             </h3>
             <br />
             {/* <blockquote className="rounded-lg bg-gray-50/80 p-2 text-gray-600 font-medium md:text-lg  text-left"> */}
             {/* <q> */}
-            <p className="text-white md:text-lg  text-left">
+            <p className="text-gray-300 md:text-lg  text-left">
               Entusiasta de la transfomacion digital, programador de codigo
               limpio y organizado, amante de las implementaciones de software
               creativas, agil en iteraciones y entrega. Mi competencia; mis
@@ -156,9 +158,220 @@ export default function Home() {
           </div>
         </section>
 
+        {/* <section className="bg-gradient-to-b from-amber-800 to-amber-900  flex flex-col justify-center items-center text-center min-h-screen">
+          <div className="px-10 py-7 rounded-lg md:w-1/2 flex-col items-center bg-gradient-to-b from-black/40 to-black/25 shadow-md border-2 border-white/20">
+            <h3 className="text-amber-300 md:text-4xl font-bold  text-left">
+              Perfil Profesional
+            </h3>
+            <br />
+            <p className="text-gray-300 md:text-lg  text-left">
+              Experto con casi 3 años de experiencia en el manejo de tecnologías
+              del ecosistema JavaScript­ principalmente con el Stack MERN y
+              NextJS. He colaborado en 7 proyectos implementando las tecnologías
+              acorde a las especificaciones, necesidades y modelo de negocio de
+              la empresa, trabajando en equipo con metodologías y rutinas agiles
+              como SCRUM y Kanban junto con buenas practicas SOLID y Clean
+              Arquitectures con TypeScript. En mis rutinas suelo utilizar
+              herramientas comunes como Figma, Notion, Trello y Postman.
+            </p>
+            <br />
+
+            <p className="text-gray-300 md:text-lg  text-left">
+              Aspiro a ser un Desarrollador Web FullStack con Node.js de alto
+              rendimiento e impacto capaz de diseñar e implementar soluciones de
+              software al mundo.
+            </p>
+            <br />
+            <hr className="opacity-20" />
+            <h3 className="text-amber-300 md:text-4xl mt-7 font-bold  text-left">
+              Cualidades y Aptitudes
+            </h3>
+            <br />
+            <p className="text-gray-300 md:text-lg  text-left">
+              Entusiasta de la transfomacion digital, programador de codigo
+              limpio y organizado, amante de las implementaciones de software
+              creativas, agil en iteraciones y entrega. Mi competencia; mis
+              habilidades para crear soluciones con las tecnologias actuales
+              impactando en un modelo de negocio que haga feliz a sus usuarios.
+            </p>
+          </div>
+        </section>
+
+        <section className="bg-gradient-to-b from-blue-900 to-blue-950  flex flex-col justify-center items-center text-center min-h-screen">
+          <div className="px-10 py-7 rounded-lg md:w-1/2 flex-col items-center bg-gradient-to-b from-black/40 to-black/25 shadow-md border-2 border-white/20">
+            <h3 className="text-blue-300 md:text-4xl font-bold  text-left">
+              Perfil Profesional
+            </h3>
+            <br />
+            <p className="text-gray-300 md:text-lg  text-left">
+              Experto con casi 3 años de experiencia en el manejo de tecnologías
+              del ecosistema JavaScript­ principalmente con el Stack MERN y
+              NextJS. He colaborado en 7 proyectos implementando las tecnologías
+              acorde a las especificaciones, necesidades y modelo de negocio de
+              la empresa, trabajando en equipo con metodologías y rutinas agiles
+              como SCRUM y Kanban junto con buenas practicas SOLID y Clean
+              Arquitectures con TypeScript. En mis rutinas suelo utilizar
+              herramientas comunes como Figma, Notion, Trello y Postman.
+            </p>
+            <br />
+
+            <p className="text-gray-300 md:text-lg  text-left">
+              Aspiro a ser un Desarrollador Web FullStack con Node.js de alto
+              rendimiento e impacto capaz de diseñar e implementar soluciones de
+              software al mundo.
+            </p>
+            <br />
+            <hr className="opacity-20" />
+            <h3 className="text-blue-300 md:text-4xl mt-7 font-bold  text-left">
+              Cualidades y Aptitudes
+            </h3>
+
+            <p className="text-gray-300 md:text-lg  text-left">
+              Entusiasta de la transfomacion digital, programador de codigo
+              limpio y organizado, amante de las implementaciones de software
+              creativas, agil en iteraciones y entrega. Mi competencia; mis
+              habilidades para crear soluciones con las tecnologias actuales
+              impactando en un modelo de negocio que haga feliz a sus usuarios.
+            </p>
+
+          </div>
+        </section>
+
         <hr className="snap-mandatorysnap-y snap-start" />
 
-        <section className="bg-gradient-to-b from-green-400 to-green-600  flex flex-col justify-center items-center text-center p-4 min-h-screen"></section>
+        <section className="bg-gradient-to-b from-blue-900 to-blue-950  flex flex-col justify-center items-center text-center min-h-screen">
+          <div className="px-10 py-7 rounded-lg md:w-1/2 flex-col items-center bg-gradient-to-b from-black/40 to-black/25 shadow-md border-2 border-white/20">
+            <h3 className="text-amber-300 md:text-4xl font-bold  text-left">
+              Perfil Profesional
+            </h3>
+            <br />
+            <p className="text-gray-300 md:text-lg  text-left">
+              Experto con casi 3 años de experiencia en el manejo de tecnologías
+              del ecosistema JavaScript­ principalmente con el Stack MERN y
+              NextJS. He colaborado en 7 proyectos implementando las tecnologías
+              acorde a las especificaciones, necesidades y modelo de negocio de
+              la empresa, trabajando en equipo con metodologías y rutinas agiles
+              como SCRUM y Kanban junto con buenas practicas SOLID y Clean
+              Arquitectures con TypeScript. En mis rutinas suelo utilizar
+              herramientas comunes como Figma, Notion, Trello y Postman.
+            </p>
+            <br />
+
+            <p className="text-gray-300 md:text-lg  text-left">
+              Aspiro a ser un Desarrollador Web FullStack con Node.js de alto
+              rendimiento e impacto capaz de diseñar e implementar soluciones de
+              software al mundo.
+            </p>
+            <br />
+            <hr className="opacity-20" />
+            <h3 className="text-amber-300 md:text-4xl mt-7 font-bold  text-left">
+              Cualidades y Aptitudes
+            </h3>
+            <br />
+
+            <p className="text-gray-300 md:text-lg  text-left">
+              Entusiasta de la transfomacion digital, programador de codigo
+              limpio y organizado, amante de las implementaciones de software
+              creativas, agil en iteraciones y entrega. Mi competencia; mis
+              habilidades para crear soluciones con las tecnologias actuales
+              impactando en un modelo de negocio que haga feliz a sus usuarios.
+            </p>
+          </div>
+        </section> */}
+
+        <hr className="snap-mandatorysnap-y snap-start" />
+
+        <section className="bg-gradient-to-b from-amber-900 to-amber-950  flex flex-col justify-center items-center text-center min-h-screen">
+          <div className="px-10 py-7 rounded-lg md:w-1/2 flex-col items-center bg-gradient-to-b from-black/40 to-black/25 shadow-md border-2 border-white/20">
+            <h3 className="text-amber-400 md:text-4xl font-bold  text-left">
+              Perfil Profesional
+            </h3>
+            <br />
+            <p className="text-gray-300 md:text-lg  text-left">
+              Experto con casi 3 años de experiencia en el manejo de tecnologías
+              del ecosistema JavaScript­ principalmente con el Stack MERN y
+              NextJS. He colaborado en 7 proyectos implementando las tecnologías
+              acorde a las especificaciones, necesidades y modelo de negocio de
+              la empresa, trabajando en equipo con metodologías y rutinas agiles
+              como SCRUM y Kanban junto con buenas practicas SOLID y Clean
+              Arquitectures con TypeScript. En mis rutinas suelo utilizar
+              herramientas comunes como Figma, Notion, Trello y Postman.
+            </p>
+            <br />
+
+            <p className="text-gray-300 md:text-lg  text-left">
+              Aspiro a ser un Desarrollador Web FullStack con Node.js de alto
+              rendimiento e impacto capaz de diseñar e implementar soluciones de
+              software al mundo.
+            </p>
+            <br />
+            <hr className="opacity-20" />
+            <h3 className="text-amber-400 md:text-4xl mt-7 font-bold  text-left">
+              Cualidades y Aptitudes
+            </h3>
+            <br />
+            {/* <blockquote className="rounded-lg bg-gray-50/80 p-2 text-gray-600 font-medium md:text-lg  text-left"> */}
+            {/* <q> */}
+            <p className="text-gray-300 md:text-lg  text-left">
+              Entusiasta de la transfomacion digital, programador de codigo
+              limpio y organizado, amante de las implementaciones de software
+              creativas, agil en iteraciones y entrega. Mi competencia; mis
+              habilidades para crear soluciones con las tecnologias actuales
+              impactando en un modelo de negocio que haga feliz a sus usuarios.
+            </p>
+            {/* </q> */}
+
+            {/* </blockquote> */}
+          </div>
+        </section>
+
+        <section className="bg-gradient-to-b from-green-900 to-green-950  flex flex-col justify-center items-center text-center min-h-screen">
+          <div className="px-10 py-7 rounded-lg md:w-1/2 flex-col items-center bg-gradient-to-b from-black/40 to-black/25 shadow-md border-2 border-white/20">
+            <h3 className="text-green-400 md:text-4xl font-bold  text-left">
+              Perfil Profesional
+            </h3>
+            <br />
+            <p className="text-gray-300 md:text-lg  text-left">
+              Experto con casi <HT>3 años</HT> de experiencia en el manejo de
+              tecnologías del ecosistema JavaScript principalmente con el{" "}
+              <HT>MERN y NextJS</HT> Stack. He colaborado en 7 proyectos
+              implementando las tecnologías acorde a las especificaciones,
+              necesidades y modelo de negocio de la empresa, trabajando en
+              equipo con metodologías y rutinas agiles como{" "}
+              <HT>SCRUM y Kanban</HT> junto con buenas practicas SOLID y Clean
+              Arquitectures con TypeScript. En mis rutinas suelo utilizar
+              herramientas comunes como <HT>Figma, Notion, Trello y Postman</HT>
+              .
+            </p>
+            <br />
+
+            <p className="text-gray-300 md:text-lg  text-left">
+              Aspiro a ser un Desarrollador Web FullStack con Node.js de alto
+              rendimiento e impacto capaz de diseñar e implementar soluciones de
+              software al mundo.
+            </p>
+            <br />
+            <hr className="opacity-20" />
+            <h3 className="text-green-500 md:text-4xl mt-7 font-bold  text-left">
+              Cualidades y Aptitudes
+            </h3>
+            <br />
+            {/* <blockquote className="rounded-lg bg-gray-50/80 p-2 text-gray-600 font-medium md:text-lg  text-left"> */}
+            {/* <q> */}
+            <p className="text-gray-300 md:text-lg  text-left">
+              Entusiasta de la transfomacion digital, programador de codigo
+              limpio y organizado, amante de las implementaciones de software
+              creativas, agil en iteraciones y entrega. Mi competencia; mis
+              habilidades para crear soluciones con las tecnologias actuales
+              impactando en un modelo de negocio que haga feliz a sus usuarios.
+            </p>
+            {/* </q> */}
+
+            {/* </blockquote> */}
+          </div>
+        </section>
+
+        {/* <section className="bg-gradient-to-b from-green-400 to-green-600  flex flex-col justify-center items-center text-center p-4 min-h-screen"></section> */}
       </main>
       <style jsx>{`
         ._font-grand-hotel {
